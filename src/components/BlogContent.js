70c7af1d8487ab0438c49c0b08327dbfc53e8d59
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-07-20 16:14:00 +0300

    Completed assignment

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  //console.log(props)
+  alert (props.articleText)
+  if (!props.isPublished){
+    return null
+  }
+  else{
+    return(
+      <div id="blog-content">
+        <h1>{props.articleText}</h1>
+        <p>{props.minutesToRead}</p>
+      </div>
+      
+    ) 
+  }
+  
 }
 
 export default BlogContent;
